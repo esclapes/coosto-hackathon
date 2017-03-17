67dbab232ab069bcd00c346b3ec1e6db73da6143
Josep <jrcamps@gmail.com>
2017-03-17 17:01:10 +0100

    Add some shitty logo

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
     <div class="header">
-      <router-link to="/">DogPlace</router-link>
+      <h1 class="page-title">
+        <router-link to="/">
+          <i class="em em-dog"></i>
+          <i class="em em-poop"></i>
+          DogPlace
+        </router-link>
+      </h1>
     </div>
     <nuxt/>
     <my-footer/>
@@ -25,6 +31,19 @@ export default {
   width: 100%;
   max-width: 1200px;
   padding: 40px 0;
+}
+
+.page-title {
+  margin: 0 25px;
+}
+
+.page-title a {
+  color: #7b481e;
+  text-decoration: none;
+}
+
+.page-title i {
+  vertical-align: bottom;
 }
 
 </style>
